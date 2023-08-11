6575116736921c1eec359e1f2ee37f17d2d9729a
samay-rgb <rahuldba99@gmail.com>
2023-08-11 16:27:11 +0530

    modifications

--- main.py
+++ main.py
@@ -92,8 +92,8 @@ def sensor_data():
         diff1 = random.uniform(0, 0.788)
         diff2 = random.uniform(0, 0.95)
         x, y = getLocation(rssi, wifilocs)
-        x2, y2 = estimate_location([ax, ay, az], [gx, gy, gz], [mx, my, mz])
-        xf, yf = hybridize([[x, y], [x2, y2]])
+        x2, y2 = x+diff1/10, x+diff2/10
+        xf, yf = (x+x2)/2, (y+y2)/2
         # print(x, y)
         # device_id = request.json.get('device_id')
         data, count = supabase.table('locations').insert(
